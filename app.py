eee8c309d0653b14cf9106034668011cef50c1be
Jan Walach <jan.walach@tuwien.ac.at>
2025-01-30 20:03:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Your App Name",
-    initial_sidebar_state="expanded",
-    layout="wide"  # optional
-)
+def main():
+    st.title("Test App")
+    st.write("Hello World!")
 
-# Titulek aplikace
-st.title("Moje Streamlit Aplikace na AWS App Runner")
-
-# Jednoduchý vstup a výstup
-name = st.text_input("Zadej své jméno:")
-if name:
-    st.write(f"Ahoj, {name}! 👋")
-
-# Tlačítko pro zobrazení zprávy
-if st.button("Klikni mě!"):
-    st.success("Gratulujeme, aplikace běží na AWS App Runner! 🚀")
-
-# Footer
-st.write("Tato aplikace běží na **Streamlit** a je hostována na **AWS App Runner**.")
+if __name__ == "__main__":
+    main()
 
